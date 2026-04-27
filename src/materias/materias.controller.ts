f836bbd36421c9a60f12d8d20206cf4871c778e7
abrahamrodmi <carlos.arodmi31@gmail.com>
2026-04-26 20:31:22 -0600

    modificacion materiascontroller

--- src/materias/materias.controller.ts
+++ src/materias/materias.controller.ts
@@ -1,34 +1,40 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { MateriasService } from './materias.service';
 import { CreateMateriaDto } from './dto/create-materia.dto';
 import { UpdateMateriaDto } from './dto/update-materia.dto';
+import { JwtGuard } from '../auth/guards/jwt.guard';
 
 @Controller('materias')
 export class MateriasController {
     constructor(private readonly materiasService: MateriasService) { }
 
+    @UseGuards(JwtGuard)
+    @HttpCode(HttpStatus.CREATED)
     @Post()
-    create(@Body() createMateriaDto: CreateMateriaDto) {
-        return this.materiasService.create(createMateriaDto);
+    async create(@Body() createMateriaDto: CreateMateriaDto, @Request() req: any) {
+        const userId = req.user.sub;
+        return this.materiasService.create(createMateriaDto, userId);
     }
 
     @Get()
-    findAll() {
+    async findAll() {
         return this.materiasService.findAll();
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string) {
+    async findOne(@Param('id') id: string) {
         return this.materiasService.findOne(id);
     }
 
+    @UseGuards(JwtGuard)
     @Patch(':id')
-    update(@Param('id') id: string, @Body() updateMateriaDto: UpdateMateriaDto) {
+    async update(@Param('id') id: string, @Body() updateMateriaDto: UpdateMateriaDto) {
         return this.materiasService.update(id, updateMateriaDto);
     }
 
+    @UseGuards(JwtGuard)
     @Delete(':id')
-    remove(@Param('id') id: string) {
+    async remove(@Param('id') id: string) {
         return this.materiasService.remove(id);
     }
 }
